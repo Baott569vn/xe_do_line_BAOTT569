235b5cd0e38415960ed5aa1f8e2f33f04f40b6b2
Baot569vn <baott569circuit@gmail.com>
2023-12-19 02:25:48 +0700

    va loi 1

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# xe_do_line_BAOTT569
+```markdown
+# Điều Khiển Robot Di Động với Cảm Biến Đường Dẫn
+
+**Tên Chương Trình:** xe_do_line_5_mat_BAOTT569.ino
+
+**Tác Giả:** BAOTT569
+
+**Ngày Sửa Đổi Cuối Cùng:** 19/12/2023
+
+## Mô Tả
+
+Chương trình xe_do_line_5_mat_BAOTT569.ino điều khiển robot di động thông qua cảm biến đường dẫn. Cung cấp khả năng thực hiện các hành động như tiến, lùi, quẹo trái và quẹo phải dựa trên thông tin cảm biến.
+
+## Hướng Dẫn Sử Dụng
+
+1. Cài đặt chương trình lên bo mạch Arduino.
+2. Kết nối cảm biến và động cơ với robot.
+3. Khởi động robot để theo dõi hành vi di chuyển.
+
+## Hàm Chính
+
+- `readSensors()`: Đọc giá trị từ cảm biến và giới hạn chúng trong khoảng 0-1000.
+- `processSensorValues()`: Chuyển đổi giá trị cảm biến thành số nhị phân.
+- `handleDirection()`: Xử lý hướng di chuyển dựa trên giá trị số nhị phân.
+- `notLine()`: Xử lý trường hợp không có đường dẫn.
+
+## Cấu Trúc
+
+- **MotorPins:** Chứa thông tin chân điều khiển động cơ.
+- **motorData:** Lưu trữ thông tin về động cơ, bao gồm chân điều khiển và tốc độ.
+
+## Báo Lỗi và Đóng Góp
+
+Đề xuất cải tiến hoặc báo lỗi tại [GitHub Issues](https://www.facebook.com/profile.php?id=100021561485210).
+
+## Giấy Phép
+
+Phần mềm được phát hành free.
+
+## Cảm ơn các bạn đã xem có thể nó không hoàng hảo nhưng rất vui vì bạn đã xem qua 😋
+---
+
+© BAOTT569 | 19/12/2023
+```
--- xe_do_line_5_mat_BAOTT569.ino
+++ xe_do_line_5_mat_BAOTT569.ino
@@ -1,137 +1,196 @@
-const size_t sensorLine[] = {A0, A1, A2, A3, A4};
-int valueTotalSen[] = {0, 0, 0, 0, 0};
+/*Mô Tả Chương Trình - Điều Khiển Robot Di Động với Cảm Biến Đường Dẫn
 
-unsigned long previousMillis = 0; // Biến để lưu giữ thời gian lần cuối cùng
-const long interval = 20;         // Thời gian chờ là 20 milliseconds (đã sửa đổi từ 1 giây)
+Tên Chương Trình: xe_do_line_5_mat_BAOTT569.ino
 
-size_t arraySize = sizeof(sensorLine) / sizeof(sensorLine[0]);
-long int convertNumber = 0;
+Tác Giả: [BAOTT569]
+
+Ngày Sửa Đổi Cuối Cùng: [19/12/2023]
+
+Mô Tả:
+Chương trình RobotControlPathSensor.ino là một đoạn mã Arduino được tạo ra để điều khiển một robot
+di động dựa trên dữ liệu đọc từ các cảm biến đường dẫn. 
+Chương trình này cung cấp khả năng cho robot thực hiện các hành động như tiến thẳng, lùi, quẹo trái, 
+và quẹo phải tùy thuộc vào thông tin thu được từ môi trường xung quanh.*/
+
+// Khai báo các chân cảm biến
+const size_t SENSOR_PINS[] = {A0, A1, A2, A3, A4};
+int sensorValues[] = {0, 0, 0, 0, 0};
+
+unsigned long previousMillis = 0;
+const long interval = 2;
+
+size_t numSensors = sizeof(SENSOR_PINS) / sizeof(SENSOR_PINS[0]);
+long int concatenatedNumber = 0;
+
+// Cấu trúc để lưu trữ chân điều khiển động cơ
+struct MotorPins
+{
+  byte en, in1, in2;
+};
+
+// Cấu trúc lưu trữ thông tin về động cơ
+struct
+{
+  MotorPins motorA{8, 7, 6};
+  MotorPins motorB{3, 5, 4};
+  byte maxSpeed = 255;
+  byte maxSpeedTurn = 240;
+} motorData;
 
 void setup()
 {
   Serial.begin(9600);
-
-  // Khởi tạo chân đầu vào và tắt tất cả các cảm biến
-  for (size_t a{0}; a < arraySize; ++a)
+  // Thiết lập chân điều khiển động cơ và cảm biến
+  pinMode(motorData.motorA.en, OUTPUT);
+  pinMode(motorData.motorA.in1, OUTPUT);
+  pinMode(motorData.motorA.in2, OUTPUT);
+  pinMode(motorData.motorB.en, OUTPUT);
+  pinMode(motorData.motorB.in1, OUTPUT);
+  pinMode(motorData.motorB.in2, OUTPUT);
+  for (size_t i{0}; i < numSensors; ++i)
   {
-    pinMode(sensorLine[a], INPUT);
-    digitalWrite(sensorLine[a], 0);
+    pinMode(SENSOR_PINS[i], INPUT);
+    digitalWrite(SENSOR_PINS[i], 0);
   }
+  delay(2000);
 }
 
-void funciton_Read_Sensor()
+void readSensors()
 {
-  // Đọc giá trị từ các cảm biến và giới hạn chúng trong khoảng 0-1000
-  for (size_t i{0}; i < arraySize; ++i)
+  // Đọc giá trị từ cảm biến và giới hạn chúng trong khoảng 0-1000
+  for (size_t i{0}; i < numSensors; ++i)
   {
-    valueTotalSen[i] = analogRead(sensorLine[i]);
-    valueTotalSen[i] = constrain(valueTotalSen[i], 0, 1000);
+    sensorValues[i] = analogRead(SENSOR_PINS[i]);
+    sensorValues[i] = constrain(sensorValues[i], 0, 1000);
   }
-  Serial.println();
+  // Serial.println();
 }
 
-void Funciton_sitution()
+void processSensorValues()
 {
-  for (size_t b{0}; b < arraySize; ++b)
+  // Chuyển đổi giá trị từ cảm biến thành số nhị phân
+  for (size_t i{0}; i < numSensors; ++i)
   {
-    // Ánh xạ giá trị cảm biến vào khoảng 0-1
-    int valueMap = map(valueTotalSen[b], 0, 1000, 0, 1);
-    convertNumber = (convertNumber * 10) + valueMap;
+    int valueMap = map(sensorValues[i], 0, 1000, 0, 1);
+    concatenatedNumber = (concatenatedNumber * 10) + valueMap;
+    /*concatenatedNumber = 0 * 10 + 1 = 1
+     concatenatedNumber = 1 * 10 + 0 = 10
+     concatenatedNumber = 10 * 10 + 1 = 101
+     concatenatedNumber = 101 * 10 + 0 = 1010
+     concatenatedNumber = 1010 * 10 + 0 = 10100*/
   }
-  Serial.println(convertNumber);
-
-  // Xử lý các tình huống dựa trên giá trị convertNumber
-  if (convertNumber == 0)
-  {
-    not_line();
-  }
-  else if (convertNumber != 0 && convertNumber != 11111)
-  {
-    if (convertNumber == 11000 || convertNumber == 11100) // queo trái
-    {
-      left_right(1, 0);
-    }
-    else if (convertNumber == 00011 || convertNumber == 00111) // queo phải
-    {
-      left_right(0, 1);
-    }
-    else if (convertNumber == 01110) // đi thẳng
-    {
-      straits_back(1, 0);
-    }
-  }
+  // Serial.println(concatenatedNumber);
 }
 
-void not_line()
+void handleDirection()
 {
-  int wait_check = 1000;
-  if (convertNumber == 00000)
+  // Xử lý hướng di chuyển dựa trên giá trị của concatenatedNumber
+  if (concatenatedNumber == 11111)
   {
-    left_right(1, 0);
-    delay(wait_check);
-    straits_back(1, 0);
-    delay(wait_check);
-    left_right(0, 1);
-    delay(wait_check);
-    straits_back(0, 1);
-    delay(wait_check);
+    notLine();
+  }
+  else if (concatenatedNumber == 10001 || concatenatedNumber == 11101 || concatenatedNumber == 0) // di thang
+  {
+    straitsBack(1, 0, 0);
+  }
+  else if (concatenatedNumber == 10100 || concatenatedNumber == 10110 || concatenatedNumber == 11110 || concatenatedNumber == 10000) // queo phai
+  {
+    leftRight(0, 1);
+  }
+  else if (concatenatedNumber == 1011 || concatenatedNumber == 1001 || concatenatedNumber == 1111 || concatenatedNumber == 1) // queo trai
+  {
+    leftRight(1, 0);
   }
 }
 
-// Hàm điều khiển khi đi thẳng hoặc lùi
-void straits_back(bool forward, bool reveres)
+void functionSituation()
 {
-  if (!forward && reveres)
-  {
-    Serial.println("thuan");
-    // Thực hiện hành động khi lùi
-    // analogWrite(3, 255);
-    // analogWrite(4, 255);
-  }
-  else if (forward && !reveres)
-  {
-    Serial.println("nghich");
-    // Thực hiện hành động khi đi thẳng
-    // analogWrite(3, -255);
-    // analogWrite(4, -255);
-  }
+  processSensorValues();
+  handleDirection();
 }
 
-// Hàm điều khiển khi rẽ trái hoặc phải
-void left_right(bool left, bool right)
+void notLine()
 {
-  if (!left && right)
+  leftRight(1, 0);
+}
+
+void driveMotors(byte in1, byte in2, byte en, MotorPins motor)
+{
+  // Điều khiển động cơ bằng cách đặt các chân đúng giá trị
+  digitalWrite(motor.in1, in1);
+  digitalWrite(motor.in2, in2);
+  analogWrite(motor.en, en);
+}
+
+void resetMotors()
+{
+  // Tắt động cơ bằng cách đặt tất cả các chân về giá trị 0
+  digitalWrite(motorData.motorA.in1, 0);
+  digitalWrite(motorData.motorA.in2, 0);
+  analogWrite(motorData.motorA.en, 0);
+  digitalWrite(motorData.motorB.in1, 0);
+  digitalWrite(motorData.motorB.in2, 0);
+  analogWrite(motorData.motorB.en, 0);
+  delay(200);
+}
+
+void straitsBack(byte forward, byte reverse, byte stop)
+{
+  // Điều khiển động cơ để di chuyển thẳng, lùi, hoặc dừng
+  if (forward == 1 && reverse == 0 && stop == 0)
   {
-    Serial.println("trai");
-    // Thực hiện hành động khi rẽ trái
-    // analogWrite(3, -255);
-    // analogWrite(4, 255);
+    // Serial.println("thuan");
+    driveMotors(0, 1, motorData.maxSpeed, motorData.motorA);
+    driveMotors(0, 1, motorData.maxSpeed, motorData.motorB);
   }
-  else if (left && !right)
+  else if (forward == 0 && reverse == 1 && stop == 0)
   {
-    Serial.println("phai");
-    // Thực hiện hành động khi rẽ phải
-    // analogWrite(3, 255);
-    // analogWrite(4, -255);
+    // Serial.println("nghich");
+    driveMotors(1, 0, motorData.maxSpeed, motorData.motorA);
+    driveMotors(1, 0, motorData.maxSpeed, motorData.motorB);
+  }
+  else if (forward == 0 && reverse == 0 && stop == 1)
+  {
+    // Serial.println("stop");
+    resetMotors();
   }
 }
 
-// Hàm đặt lại biến cờ convertNumber
+void leftRight(byte left, byte right)
+{
+  // Điều khiển động cơ để rẽ trái hoặc phải
+  if (left == 1 && right == 0)
+  {
+    // Serial.println("trai");
+    driveMotors(0, 1, motorData.maxSpeed, motorData.motorA);
+    driveMotors(1, 0, motorData.maxSpeedTurn, motorData.motorB);
+  }
+  else if (left == 0 && right == 1)
+  {
+    // Serial.println("phai");
+    driveMotors(1, 0, motorData.maxSpeedTurn, motorData.motorA);
+    driveMotors(0, 1, motorData.maxSpeed, motorData.motorB);
+  }
+  return;
+}
+
 void resetVariable()
 {
-  convertNumber = 0;
+  // Đặt lại biến và tắt động cơ
+  leftRight(0, 0);
+  straitsBack(0, 0, 0);
+  concatenatedNumber = 0;
 }
 
 void loop()
 {
-  unsigned long currentMillis = millis(); // Lấy thời gian hiện tại
+  unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval)
   {
-    funciton_Read_Sensor();
-    Funciton_sitution();
-    // Cập nhật thời gian lần cuối cùng
+    readSensors();
+    functionSituation();
+    resetVariable();
     previousMillis = currentMillis;
   }
-  resetVariable();
 }
